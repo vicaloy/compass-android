34bbab39702569ac1256c398c13834883dd1e912
victoriaaloy <vic.aloy@gmail.com>
2024-07-16 07:32:11 -0300

    Refactor

--- app/src/main/java/com/valoy/compass/presentation/screens/navigation/NavSections.kt
+++ app/src/main/java/com/valoy/compass/presentation/screens/navigation/NavSections.kt
@@ -1,6 +1,6 @@
 package com.valoy.compass.presentation.screens.navigation
 
-enum class NavSections(section: String) {
-    EMOJI_RESULT("result"),
-    EMOJI_SEARCH("search")
+enum class NavSections() {
+    EMOJI_RESULT,
+    EMOJI_SEARCH
 }
